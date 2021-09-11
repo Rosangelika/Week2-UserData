d3337ed911815d0afcddde424bd5faab550f3565
Rosangelika Todoruk <rtodoruk@tktk.ee>
2021-09-11 23:33:26 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust; 
+            //programm arvutab kasutaja sünniaastat; 
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal 1996"
+
+
+            Console.WriteLine("Tere! Esimene iseseisev töö katsetus.");
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+
+            int dateOfAge = Convert.ToInt32(Console.ReadLine());
+
+            int userYear = 2021 - dateOfAge;
+
+            //Console.WriteLine("Oled " + userYear + " aastal sündinud.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userYear} aastal sündinud.");
+
+
+
+
+        }
+    }
+}
---
+++ UserData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pragramm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust kosnoolist;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+        }
+    }
+}
